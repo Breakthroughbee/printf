45de52d4b5527bf3d35cbfd94145be5d6cddf413
p-prime1 <preciousnwaka95@gmail.com>
2024-01-29 16:10:29 +0100

    a

--- printf.c
+++ printf.c
@@ -6,28 +6,25 @@
 
 int _printf(const char *format, ...)
 {
-	unsigned int i,j;
-	char c;
-	char *str;
+	int i;
+/*	char c;
+	char *str;*/
 	va_list args;
-
 	i = 0;
-	j = 0;
-	va_start(args, format);
+	/*j = 0;*/
+	va_start(args, *format);
 	while (format[i] == '\0')
 	{
-		if (format[i] == '%')
+	/*	if (format[i] == '%')
 		{
 			if (format[i + 1] == '%')
 			{
 				_putchar('%');
-				i++;
 			}
 			else if (format[i + 1] == 'c')
 			{
 				c = va_arg(args, int);
 				_putchar(c);
-				i++;
 			}
 			else if (format[i + 1] == 's')
 			{
@@ -37,10 +34,15 @@ int _printf(const char *format, ...)
 					_putchar(str[j]);
 					j++;
 				}
-				i++;
 			}
 			i++;
 		}
+		else
+		{
+			*/
+			_putchar(format[i]);
+			i++;
+	/*	}*/
 	}
 	va_end(args);
 	return (0);
